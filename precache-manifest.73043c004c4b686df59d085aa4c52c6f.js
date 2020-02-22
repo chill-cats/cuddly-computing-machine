cf89682e01469ef7a9bee11376e466a3c9e3c0d1
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2020-02-22 16:00:28 +0000

    Deploying to gh-pages from master - 1ad7917a280c0042f90bd42dc4d489e206ed45e1 🚀

---
+++ precache-manifest.73043c004c4b686df59d085aa4c52c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b714a702f83fa1f8aab4add2f5114b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1a241ffb584ea3ace6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a4f3667dfeb850ccc9d",
+    "url": "/static/js/2.95e72608.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.95e72608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1a241ffb584ea3ace6",
+    "url": "/static/js/main.a73f546e.chunk.js"
+  },
+  {
+    "revision": "3efafafe824be77ac41d",
+    "url": "/static/js/runtime-main.d350d3de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
