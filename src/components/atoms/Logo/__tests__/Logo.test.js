cc56d24ec3a75c7958086fcf74ad9454c7d8274a
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2020-02-23 08:50:48 +0700

    Add dump test case

--- src/components/atoms/Logo/__tests__/Logo.test.js
+++ src/components/atoms/Logo/__tests__/Logo.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Logo from "../Logo";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 test("render logo to my Name", () => {
   const { getByText } = render(<Logo>Le Nguyen Hoang Nhan</Logo>);
   const h1Elem = getByText(/Le Nguyen Hoang Nhan/i);
   expect(h1Elem).toBeInTheDocument();
+});
+test("render children tag", () => {
+  render(
+    <Logo>
+      <em>Hello</em>
+    </Logo>
+  );
+  expect(screen.queryByText("Hello")).toBeInTheDocument();
 });
